9624a6891a6cb3ad51233f04315aeb2efba5c6e2
shunsuke <shuntanaka01@outlook.jp>
2024-09-05 09:41:37 +0900

    [fix]タイトル

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
-  const [helloText, setHelloText] = useState<string>()
-  
-  return <>
-    <h1>
-      Hello App
-    </h1>
-    <button onClick={async () => {
-      const response = await fetch(`${process.env.REACT_APP_API_SERVER}/hello`, {mode: 'cors'})
-      const text = await response.text()
-      setHelloText(text)
-    }}>
-      Call API
-    </button>
-    <div>
-      {helloText}
-    </div>
-  </>
+  const [helloText, setHelloText] = useState<string>();
+
+  return (
+    <>
+      <h1>Hello App2</h1>
+      <button
+        onClick={async () => {
+          const response = await fetch(
+            `${process.env.REACT_APP_API_SERVER}/hello`,
+            { mode: 'cors' }
+          );
+          const text = await response.text();
+          setHelloText(text);
+        }}
+      >
+        Call API
+      </button>
+      <div>{helloText}</div>
+    </>
+  );
 }
 
-export default App
+export default App;
